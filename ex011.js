e140be0225b74f473f6625bea098c7079acbde5f
wallames1 <wallames@live.com>
2022-08-25 01:21:06 -0300

    idade jv

---
+++ ex011.js
@@ -0,0 +1,11 @@
+var idade = 77
+if (idade < 16) {
+    console.log ('não vota')
+} else {
+    if (idade < 18 || idade > 65) {
+        console.log('voto opcional')
+    }
+    else {
+        console.log('voto obrigatorio')
+    }
+}
